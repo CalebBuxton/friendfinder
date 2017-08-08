86115e929cf5ab87a3e2b8da0c9c8a6ba796897e
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-08-08 10:34:49 -0400

    added test data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,17 @@
-var arr = [];
+var arr = [
+{
+	id: 1,
+	name: "Patrick Star",
+	photo: "https://res.cloudinary.com/teepublic/image/private/s--G-sEUdGD--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1446161353/production/designs/71233_0.jpg",
+	scores: [5,5,5,5,1,5,5,1,1,5]
+},
+
+{
+	id: 2,
+	name: "Chuck Norris",
+	photo: "http://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg",
+	scores: [1,1,5,1,5,1,1,1,1,1]
+}
+];
 
 module.exports = arr;
