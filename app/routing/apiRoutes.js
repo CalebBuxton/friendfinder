73aa8ae9228ea81c2c4db4b2d59abeae9ee0818e
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-08-08 09:37:30 -0400

    functional app for Heroku deployment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,24 +2,15 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		res.json(reserved);
+		res.json(friends);
 	});
 
 	app.post("/api/friends", function(req, res) {
-		if (reserved.length < 5) {
-			reserved.push(req.body);
+			friends.push(req.body);
 			res.json(true);
-		}
-		else {
-			waiting.push(req.body);
-			res.json(false);
-		}
 	});
 
 	app.post("/api/clear", function(req, res) {
-		reserved = [];
-		waiting = [];
-
-		console.log(reserved)
+		friends = [];
 	})
 }
