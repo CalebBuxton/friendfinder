9b6fd673c2a9bdb9efe84bdac169fb62d7db0b37
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-08-08 11:33:28 -0400

    added myself to the test data

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,6 +11,24 @@ var arr = [
 	name: "Chuck Norris",
 	photo: "https://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg",
 	scores: [1,1,5,1,5,1,1,1,1,1]
+},
+
+{
+"id": "3",
+"name": "Caleb Buxton",
+"photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAiiAAAAJDhiYTQyMjlmLTc5YjktNGExNi04NDExLWU4ZTBkM2U0ZjA3Mg.jpg",
+"scores": [
+"2",
+"3",
+"1",
+"1",
+"4",
+"1",
+"4",
+"5",
+"3",
+"5"
+]
 }
 ];
 
